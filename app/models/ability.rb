56298ad2aaad66a6431b715d041eb7a8c88a0468
Zach Fountain <zach.fountain@gmail.com>
2015-08-18 00:57:43 -0500

    Authorization, button text updates

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,10 +5,13 @@ class Ability
     user ||= User.new # guest user (not logged in)
     if user.role == "admin"
       can :manage, :all
-    else
+    elsif user.id
       can :manage, user
       can :manage, Encounter, :user_id => user.id
       can :update, Cryptid
+      can :read, Encounter
+      can :read, Cryptid
+    else
       can :read, Encounter
       can :read, Cryptid
     end
